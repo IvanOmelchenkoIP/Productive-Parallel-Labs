6bd84c927aeb093550d726415681be9a91633067
IvanOmelchenkoIP <io25112002@gmail.com>
2023-03-05 18:20:15 +0200

    Add needed commentary
    Add needed commentary for task

--- lab0/src/lab0/Data/FileReader.java
+++ lab0/src/lab0/Data/FileReader.java
@@ -1,3 +1,14 @@
+/*
+ * Лабораторна робота 0 ЛР0, Варіанти - 1.14, 2.23, 3.3
+ * F1: D = (SORT(A + B) + C) * (MA * MB)
+ * F2: q = MAX(MH * MK - ML)
+ * F3: O = SORT(P)*(MR*MT)
+ * Омельченко І. ІП-04
+ * Дата відправлення: 06.03.2023 
+ * 
+ * файл: ./src/lab0/data/FileReader.java
+ */
+
 package lab0.Data;
 
 import java.io.DataInputStream;
